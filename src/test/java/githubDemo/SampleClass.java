d693181ab374a0ca3bfe2ac99caec5b3d129ad3f
Dhiraj0991 <59369141+Dhiraj0991@users.noreply.github.com>
2020-04-03 22:42:56 +0530

    First Commit...

---
+++ src/test/java/githubDemo/SampleClass.java
@@ -0,0 +1,17 @@
+package githubDemo;
+
+public class SampleClass
+{
+
+	public static void main(String[] args)
+	{
+		int data[]= {0,4,42};
+		
+		for (int i = 0; i < data.length; i++)
+		{
+			System.out.println(data[i]);
+		}
+
+	}
+
+}
