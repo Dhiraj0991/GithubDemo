e48c8c7e17ec9c543a05775d618bda175c509528
Dhiraj0991 <59369141+Dhiraj0991@users.noreply.github.com>
2020-04-03 23:14:00 +0530

    added sum values

--- src/test/java/githubDemo/SampleClass.java
+++ src/test/java/githubDemo/SampleClass.java
@@ -6,11 +6,13 @@ public class SampleClass
 	public static void main(String[] args)
 	{
 		int data[]= {0,4,42};
-		
+		int sum=0;
 		for (int i = 0; i < data.length; i++)
 		{
 			System.out.println(data[i]);
+			sum+=data[i];
 		}
+		System.out.println(sum);
 
 	}
 
